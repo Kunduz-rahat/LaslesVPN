8324f9dec25910dc61cb0b6849cafb6e8b2b1c89
Kunduz Rahat <rahatova.kunduz@gmail.com>
2021-06-23 18:16:30 +0300

    last edition

--- js/script.js
+++ js/script.js
@@ -1,29 +1,29 @@
-let modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.addEventListener('click',  () =>  {
-    modal.style.display = "block";
-})
-
-// When the user clicks on <span> (x), close the modal
-span.addEventListener('click', () =>   {
-    modal.style.display = "none";
-})
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', (event) =>  {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-})
-let hisBtn = document.querySelector('#hisBtn')
-
-hisBtn.addEventListener('click', () => {
-    console.log('<<<<')
-})
+// let modal = document.getElementById("myModal");
+//
+// // Get the button that opens the modal
+// let btn = document.getElementById("myBtn");
+//
+// // Get the <span> element that closes the modal
+// let span = document.getElementsByClassName("close")[0];
+//
+// // When the user clicks on the button, open the modal
+// btn.addEventListener('click',  () =>  {
+//     modal.style.display = "block";
+// })
+//
+// // When the user clicks on <span> (x), close the modal
+// span.addEventListener('click', () =>   {
+//     modal.style.display = "none";
+// })
+//
+// // When the user clicks anywhere outside of the modal, close it
+// window.addEventListener('click', (event) =>  {
+//     if (event.target === modal) {
+//         modal.style.display = "none";
+//     }
+// })
+// let hisBtn = document.querySelector('#hisBtn')
+//
+// hisBtn.addEventListener('click', () => {
+//     console.log('<<<<')
+// })
